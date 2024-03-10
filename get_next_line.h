6396cae58740fd9f19e3a39a569ad773aebfe035
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2024-03-10 15:38:09 +0100

    added utils functions

--- get_next_line.h
+++ get_next_line.h
@@ -1,5 +1,11 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# include <stdlib.h>
+
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *str);
 
 #endif
 
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,1 +1,81 @@
 #include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str[len])
+	{
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len;
+	char	*res;
+	char	*res_ptr;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len = ft_strlen((char *)s1) + ft_strlen((char *)s2);
+	res = malloc(sizeof(char) * len + 1);
+	if (!res)
+		return (NULL);
+	res_ptr = res;
+	while (*s1)
+	{
+		*res++ = *s1++;
+	}
+	while (*s2)
+	{
+		*res++ = *s2++;
+	}
+	*res = '\0';
+	return (res_ptr);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	unsigned char	*s_cpy;
+
+	s_cpy = (unsigned char *)s;
+	while (*s_cpy)
+	{
+		if (*s_cpy == (unsigned char)c)
+		{
+			return ((char *)s_cpy);
+		}
+		s_cpy++;
+	}
+	if (*s_cpy == '\0' && (unsigned char)c == '\0')
+		return ((char *)s_cpy);
+	return (NULL);
+}
+
+char	*ft_strdup(const char *str)
+{
+	int		i;
+	char	*dup;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	dup = (char *)malloc(sizeof(char) * (i + 1));
+	if (!dup)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (str[i])
+	{
+		dup[i] = str[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
